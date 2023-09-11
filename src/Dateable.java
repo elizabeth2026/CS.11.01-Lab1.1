acb98c3afb2483bc346fa6d32c772c6bf177f1e4
elizabeth2026 <elizabethl2026@student.cis.edu.hk>
2023-09-11 11:31:53 +0800

    labs

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        double halfage= (double) age/2.0;
+        double dateableAge = (double) halfage+7.0;
+        System.out.println(age +" year old's should date somebody who is at least " + dateableAge + " years old");
     }
 }
